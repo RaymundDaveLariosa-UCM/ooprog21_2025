30adf77401ceaa6ce2a88e0ca3a002c310a59835
RaymundDaveLariosa-UCM <raymundlariosa123@gmail.com>
2025-09-16 09:03:39 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,16 @@
+public class Comments {
+    // This is a single-line comment
+
+    /*
+     This is a
+     multi-line comment
+    */
+
+    /**
+     * This is a documentation comment
+     * Used to describe the class or method
+     */
+    public static void main(String[] args) {
+        System.out.println("This program demonstrates the 3-different types of comments in Java.");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("It's beauty in the struggle, ugliness in the success");
+        System.out.println("Hear my words and listen to my signal of distress");
+        System.out.println("I grew up in the city and know sometimes we had less");
+        System.out.println("Compared to some my homies down the block, man, we were blessed");
+        
+            }
+}
